603640f3e324e5d5738ea1c885581edd9172fa72
olearytd <timothy.oleary@maxar.com>
2026-01-09 18:08:20 -0500

    init xcode project

---
+++ ios/nurture-fox/nurture-fox/nurture_foxApp.swift
@@ -0,0 +1,32 @@
+//
+//  nurture_foxApp.swift
+//  nurture-fox
+//
+//  Created by Tim OLeary on 1/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct nurture_foxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ios/nurture-fox/nurture-foxTests/nurture_foxTests.swift
@@ -0,0 +1,17 @@
+//
+//  nurture_foxTests.swift
+//  nurture-foxTests
+//
+//  Created by Tim OLeary on 1/9/26.
+//
+
+import Testing
+@testable import nurture_fox
+
+struct nurture_foxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
